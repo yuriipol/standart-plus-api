f9a4a29720c45c8143763699f2b40dce9dba98e8
unknown <polupan85@gmail.com>
2023-06-22 19:50:08 +0300

    add console.log

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const http = require("http");
 
 http
   .createServer(function (request, response) {
+    
     console.log(request.url);
+    console.log("🚀 ~ file: app.js:5 ~ response:")
+    
     if (request.url === "/") {
       response.end("Main");
     } else if (request.url === "/category") {
