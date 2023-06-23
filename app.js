1bfc98f2df55052567e697423100dc468576ae94
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-06-23 10:29:50 +0300

    add files

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const http = require("http");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-http
-  .createServer(function (request, response) {
-    console.log(request.url);
-    if (request.url === "/") {
-      response.end("Main");
-    } else if (request.url === "/category") {
-      response.end("category");
-    }
-  })
-  .listen(3000);
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
